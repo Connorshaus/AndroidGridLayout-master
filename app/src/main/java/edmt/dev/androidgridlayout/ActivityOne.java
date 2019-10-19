98ce50a1d2ac1c3b69521ebc337853665eddd114
Connor <connorshaus@gmail.com>
2019-10-18 23:37:42 -0400

    Finished Register and Login Activities. Added Logout button to My Profile Tab

--- app/src/main/java/edmt/dev/androidgridlayout/ActivityOne.java
+++ app/src/main/java/edmt/dev/androidgridlayout/ActivityOne.java
@@ -1,8 +1,12 @@
 package edmt.dev.androidgridlayout;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class ActivityOne extends AppCompatActivity {
 
@@ -10,5 +14,11 @@ public class ActivityOne extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_one);
+    }
+
+    public void logout (View view){
+        FirebaseAuth.getInstance().signOut();
+        startActivity(new Intent(getApplicationContext(), Login.class));
+        finish();
     }
 }
--- app/src/main/java/edmt/dev/androidgridlayout/Register.java
+++ app/src/main/java/edmt/dev/androidgridlayout/Register.java
@@ -1,13 +1,95 @@
 package edmt.dev.androidgridlayout;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.FirebaseApiNotAvailableException;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class Register extends AppCompatActivity {
+    EditText mFullName, mEmail, mPassword, mPhone;
+    Button mRegisterbtn, mLoginBtn;
+    FirebaseAuth fAuth;
+    ProgressBar progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        mFullName   = findViewById(R.id.FullName);
+        mEmail      = findViewById(R.id.email);
+        mPassword   = findViewById(R.id.password);
+        mPhone      = findViewById(R.id.phone);
+        mRegisterbtn= findViewById(R.id.regbutton);
+        mLoginBtn   = findViewById(R.id.login);
+
+        fAuth       = FirebaseAuth.getInstance();
+        progressBar = findViewById(R.id.progressBar);
+
+        if(fAuth.getCurrentUser() != null) {
+            startActivity(new Intent(getApplicationContext(), MainActivity.class));
+            finish();
+        }
+
+
+        mRegisterbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View V) {
+                String email = mEmail.getText().toString().trim();
+                String password = mPassword.getText().toString().trim();
+
+                if(TextUtils.isEmpty(email)) {
+                    mEmail.setError("Please provide an email.");
+                    return;
+                }
+                if(TextUtils.isEmpty(password)) {
+                    mPassword.setError("Please provide a password.");
+                    return;
+                }
+
+                if(password.length() < 6) {
+                    mPassword.setError("Password must be greater than 6 characters long.");
+                    return;
+                }
+
+                progressBar.setVisibility(View.VISIBLE);
+
+                // Register the user in Firebase
+
+                fAuth.createUserWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()) {
+                            Toast.makeText(Register.this, "User Created", Toast.LENGTH_SHORT).show();
+                            startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                        }
+                        else {
+                            Toast.makeText(Register.this, "Error " + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                            progressBar.setVisibility(View.GONE);
+                        }
+                    }
+                });
+            }
+        });
+
+        mLoginBtn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick (View v) {
+                startActivity(new Intent(getApplicationContext(), Login.class));
+            }
+        });
     }
 }
