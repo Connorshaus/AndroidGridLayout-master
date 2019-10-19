7ace9f0fe23fdbbd18ade1fe68950f2a9778175f
jj16d <32371991+jj16d@users.noreply.github.com>
2019-10-19 14:11:05 -0400

    Added string resources for main activity and text for about us page.

--- app/src/main/java/edmt/dev/androidgridlayout/AboutUs.java
+++ app/src/main/java/edmt/dev/androidgridlayout/AboutUs.java
@@ -1,7 +1,10 @@
 package edmt.dev.androidgridlayout;
 
+import android.renderscript.Element;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import static java.lang.Math.E;
 
 public class AboutUs extends AppCompatActivity {
 
@@ -9,5 +12,6 @@ public class AboutUs extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about_us);
+
     }
 }
