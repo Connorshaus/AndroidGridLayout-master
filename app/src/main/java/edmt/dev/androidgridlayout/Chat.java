6f4bf216959856e7d54d6c0ca14aae90812ff990
cooperhauck <56132358+cooperhauck@users.noreply.github.com>
2019-10-20 19:28:15 -0400

    Update to messaging: Linked to firebase database implement send button

--- app/src/main/java/edmt/dev/androidgridlayout/Chat.java
+++ app/src/main/java/edmt/dev/androidgridlayout/Chat.java
@@ -2,12 +2,53 @@ package edmt.dev.androidgridlayout;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.view.View;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
 
 public class Chat extends AppCompatActivity {
 
+    private DatabaseReference myRef;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat);
+
+
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        myRef = database.getReference("message");
+
+        final TextView myText = findViewById(R.id.Text);
+
+        // Read from the database
+        myRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                myText.setText(dataSnapshot.getValue().toString());
+            }
+
+            @Override
+            public void onCancelled(DatabaseError error) {
+                // Failed to read value
+                myText.setText("CANCELLED");
+            }
+        });
+
     }
+
+
+
+    public void sendMessage(View view){
+        EditText newText = findViewById(R.id.editText);
+        myRef.setValue(newText.getText().toString());
+        newText.setText("");
+    }
+
 }
